6fc29f1a83558acb60bb1c4c77ef54e35d48e918
Dylan Turnbull <dylanturn@gmail.com>
2020-11-23 21:44:31 -0600

    Updated the permissions

--- kergiva-main.tf
+++ kergiva-main.tf
@@ -16,7 +16,7 @@ resource "kubernetes_secret" "secret" {
 }
 
 module "project" {
-  source = "github.com/turnbros/terraform-argocd-project?ref=v1.0.0"
+  source = "github.com/project-octal/terraform-argocd-project?ref=v1.0.1"
 
   argocd_namespace = data.terraform_remote_state.infra.outputs.cluster_argocd_namespace
   name             = local.instance_name
@@ -27,17 +27,19 @@ module "project" {
       namespace = kubernetes_namespace.rational_healthcare.metadata.0.name
     }
   ]
-  oidc_group_role_membership = {
-    "argocd-kergiva-developers" = [
-      "argocd-kergiva-developers"
-    ]
-  }
-  roles = [
+  permissions = [
     {
-      name = "argocd-kergiva-developers"
-      description = "A role for the developers of Kergiva"
+      name = "developer-read-only"
+      description = "A read-only role for the developers of Kergiva"
       policies = [
-        "p, proj:${local.instance_name}:argocd-kergiva-developers, applications, get, ${local.instance_name}/*, allow"
+        {
+          resource = "applications"
+          action = "get"
+          object = "*"
+        }
+      ]
+      oidc_groups = [
+        "argocd-kergiva-developers"
       ]
     }
   ]
